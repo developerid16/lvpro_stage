38338c2b1f3e618fc7970aefb45c1b89bd45f955
Dev <dev@dev.com>
2025-11-04 13:23:28 +0530

    Color issue

--- resources/views/layouts/master-without-nav.blade.php
+++ resources/views/layouts/master-without-nav.blade.php
@@ -7,6 +7,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+          @php
+        if (Cache::has('CMScolor')) {
+
+            $color = Cache::get('CMScolor', '#FFC0CB');
+        } else {
+            $color = App\Models\ContentManagement::where('name', 'CMScolor')->value('value') ?? '#FFC0CB';
+            Cache::put('CMScolor', $color);
+        }
+
+    @endphp
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.png')}}">
         @include('layouts.head-css')
