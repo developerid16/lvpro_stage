ed63aea563f2efdc16847723ea427f5f0edb2760
Dev <dev@dev.com>
2025-12-19 13:27:31 +0530

    changes done

--- app/Http/Controllers/Admin/CsoIssuanceController.php
+++ app/Http/Controllers/Admin/CsoIssuanceController.php
@@ -103,7 +103,12 @@ class CsoIssuanceController extends Controller
 
             $action .= "</div>";
 
-
+            
+            $rewardType = match ((int) ($row->reward->reward_type ?? 0)) {
+                0 => 'Digital',
+                1 => 'Physical',
+                default => '-',
+            };
 
             $final_data[] = [
                 'sr_no'               => $index,
@@ -112,7 +117,7 @@ class CsoIssuanceController extends Controller
                 'member_id'           => $row->member_id,
                 'qty'                 => $row->qty,
                 'payment_mode'        => strtoupper($row->payment_mode),
-                'reward_type'          => ((int) ($row->reward->reward_type ?? 1) === 0) ? 'Digital' : 'Physical',
+                'reward_type'          => $rewardType,
                 'status'              => $status,
                 'receipt_datetime'    => optional($row->created_at)->format(config('shilla.date-format')),
                 'redeemed_datetime' => $row->redeemed_at ? $row->redeemed_at->format(config('shilla.date-format')) : '-',
@@ -134,13 +139,19 @@ class CsoIssuanceController extends Controller
         $purchase = Purchase::with('reward')->findOrFail($id);
         $statusData = $this->purchaseStatus($purchase->status);
     
+        $rewardType = match ((int) ($purchase->reward->reward_type ?? 0)) {
+            0 => 'Digital',
+            1 => 'Physical',
+            default => '-',
+        };
+
         return response()->json([
             'receipt_no'        => $purchase->receipt_no,
             'reward_name'       => $purchase->reward->name ?? '-',
             'member_id'         => $purchase->member_id,
             'qty'               => $purchase->qty,
             'payment_mode'      => strtoupper($purchase->payment_mode),
-            'reward_type'          => ((int) ($row->reward->reward_type ?? 1) === 0) ? 'Digital' : 'Physical',
+            'reward_type' => $rewardType,
              'status_badge' => "<span class='badge bg-{$statusData['class']}'>
                         {$statusData['label']}
                    </span>",
