785ba28e8d7b11a5794c4e88faa9453415ffcf8f
Dev <dev@dev.com>
2025-12-20 12:13:32 +0530

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -148,7 +148,7 @@
         const msalConfig = {
             auth: {
                 clientId: "{{ config('services.azure.client_id') }}",
-                authority: "https://login.microsoftonline.com/organizations"
+                authority: "https://login.microsoftonline.com/organizations",
                 redirectUri: "{{ url('/auth/callback') }}"
             }
         };
