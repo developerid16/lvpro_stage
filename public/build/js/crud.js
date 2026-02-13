4e0d6936194109e0fc0ce536039d51e1b5582919
Dev <dev@dev.com>
2026-02-13 10:28:09 +0530

    fix: optimize handling of participating locations in edit modal

--- public/build/js/crud.js
+++ public/build/js/crud.js
@@ -86,13 +86,12 @@ $(document).ready(function () {
             data: '',
             headers: { 'X-CSRF-Token': csrf },
             success: function (response) {
-                let selectedIds = response.participatingLocations.map(l => l.id);
-                loadParticipatingMerchantLocations($("#EditModal"), selectedIds);
-                window.savedLocations = response.savedLocations || {};
-
                 if (response && response.participatingLocations && response.participatingLocations.length > 0){
+                    let selectedIds = response.participatingLocations.map(l => l.id);
+                    loadParticipatingMerchantLocations($("#EditModal"), selectedIds);
                     participatingLocations = response.participatingLocations;
                 }
+                window.savedLocations = response.savedLocations || {};
                 
                 $("body").append(response.html);
                 $("#EditModal .select2").select2({
