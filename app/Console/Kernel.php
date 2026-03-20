851a256bcfcf5990953925018c8c9c25f2137da5
Mayur <mayur.ciesto@gmail.com>
2026-03-20 11:51:48 +0530

    update kernal
    update kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,40 +30,39 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        // $schedule->command('reward:expired')->dailyAt('23:55');
-        // $schedule->command('delete:oldreport')->dailyAt('00:01');
-        // $schedule->command('customer:report')->dailyAt('01:00');
-        // $schedule->command('expired:notification')->dailyAt('00:05');
-        // // $schedule->command('broadcast:notification')->everyMinute();
-        // $schedule->command('spent:reset')->yearlyOn(9, 1, '00:02');
-        // $schedule->command('key:expiry')->yearlyOn(12, 1, '00:02');
-        // $schedule->command('locations:disable-expired')->daily();
+        $schedule->command('reward:expired')->dailyAt('23:55');
+        $schedule->command('delete:oldreport')->dailyAt('00:01');
+        $schedule->command('customer:report')->dailyAt('01:00');
+        $schedule->command('expired:notification')->dailyAt('00:05');
+        // $schedule->command('broadcast:notification')->everyMinute();
+        $schedule->command('spent:reset')->yearlyOn(9, 1, '00:02');
+        $schedule->command('key:expiry')->yearlyOn(12, 1, '00:02');
+        $schedule->command('locations:disable-expired')->daily();
 
-        // $schedule->command('member:basic-detail-modified')->everyFiveMinutes();
-        $schedule->command('member:basic-detail-ig')->everyFiveMinutes();
+        $schedule->command('member:basic-detail-modified')->daily();
 
-        // $schedule->command('member:basic-detail-ig')->daily();
+        $schedule->command('member:basic-detail-ig')->daily();
 
-        // $schedule->command('member:latest-transaction')->daily();
+        $schedule->command('member:latest-transaction')->daily();
 
-        // $schedule->command('member:customer-zone')->daily();
+        $schedule->command('member:customer-zone')->daily();
 
-        // $schedule->command('ax6:get-srp-merchandise-item-list')->daily();
+        $schedule->command('ax6:get-srp-merchandise-item-list')->daily();
 
-        // // $schedule->command('master:sync-all')->dailyAt('00:00'); // call manully php artisan master:sync-all
+        // $schedule->command('master:sync-all')->dailyAt('00:00'); // call manully php artisan master:sync-all
 
+        $schedule->command('master:sync-gender')->daily();
         // $schedule->command('master:sync-gender')->daily();
-        // // $schedule->command('master:sync-gender')->daily();
-        // $schedule->command('master:sync-marital-status')->daily();
-        // $schedule->command('master:sync-card-type')->daily();
-        // $schedule->command('master:sync-dependent-type')->daily();
-        // $schedule->command('master:sync-zone')->daily();
-        // $schedule->command('master:sync-membership-code')->daily();
-        // $schedule->command('master:sync-interest-group')->daily();
+        $schedule->command('master:sync-marital-status')->daily();
+        $schedule->command('master:sync-card-type')->daily();
+        $schedule->command('master:sync-dependent-type')->daily();
+        $schedule->command('master:sync-zone')->daily();
+        $schedule->command('master:sync-membership-code')->daily();
+        $schedule->command('master:sync-interest-group')->daily();
 
 
-        // $schedule->command('users:auto-logout')->everyFiveMinutes();
-        // $schedule->command('voucher:generate-next-year')->yearlyOn(12, 31, '23:59');
+        $schedule->command('users:auto-logout')->everyFiveMinutes();        
+        $schedule->command('voucher:generate-next-year')->yearlyOn(12, 31, '23:59');
     }
 
     /**
