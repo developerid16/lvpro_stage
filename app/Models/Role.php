88449680b7aa7eff47400e89b5c8836ecf62881d
Mayur <mayur.ciesto@gmail.com>
2026-03-25 15:14:14 +0530

    role-module-add-softdelete

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
+    use SoftDeletes;
     public function department()
     {
         return $this->belongsTo(Department::class, 'department');
